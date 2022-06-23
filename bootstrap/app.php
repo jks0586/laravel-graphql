7642e0a7ae084994c767371bff6e093c85fdf4af
jitendra ofice <jks0586@gmail.com>
2022-06-23 17:45:21 +0530

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+// $app->register(MLL\GraphQLPlayground\GraphQLPlaygroundServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Category;
+use App\Models\Quest;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Category::factory(10)->create();
+        Quest::factory(10)->create();
     }
 }
